dc692c8812168c2a9bfeb09b3cd68e39617b9989
yeonseong <yeonseong.dev@gmail.com>
2023-03-18 19:04:53 +0900

    INIT: 초기세팅

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,74 @@
+<script>
+	import Header from "./Header.svelte";
+	import { onMount } from "svelte";
+	import { createScene } from "../lib/scene.svelte";
+
+	/**
+	 * @type {HTMLCanvasElement}
+	 */
+	let canvas_name;
+	onMount(() => {
+		createScene(canvas_name);
+	});
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+	<canvas bind:this={canvas_name} />
+
+	<footer>
+		<p>
+			42 베네핏에 대해 설명하는 글 <a href="https://kit.svelte.dev"
+				>노션 링크</a
+			> 컨퓨리튜트 안내글
+		</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	canvas {
+		position: absolute;
+		top: 0;
+		left: 0;
+		z-index: -1;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>42 benefit</title>
+	// TODO: add meta description detail
+	<meta name="description" content="42 benefit" />
+</svelte:head>
+
+<h1>this is the world</h1>
