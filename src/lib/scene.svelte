2e2ab10c7a0d4f72486b7704c8dce61fd00f0866
yeonseong <yeonseong.dev@gmail.com>
2023-03-18 21:38:58 +0900

    WIP: water

--- src/lib/scene.svelte
+++ src/lib/scene.svelte
@@ -27,6 +27,23 @@
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
 
+  const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+  const waterMaterial = {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: new THREE.TextureLoader().load(
+      "https://threejs.org/examples/textures/waternormals.jpg",
+      function (texture) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      }
+    ),
+    alpha: 1.0,
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined,
+  };
   const water = new Water(waterGeometry, waterMaterial);
   water.rotation.x = -Math.PI / 2;
   scene.add(water);
