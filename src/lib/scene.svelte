d0698be06db3cdcf58199519b66d66fff05d597d
yeonseong <yeonseong.dev@gmail.com>
2023-03-18 21:33:15 +0900

    FEAT: update sun and sky

--- src/lib/scene.svelte
+++ src/lib/scene.svelte
@@ -2,9 +2,10 @@
   // @ts-nocheck
 
   import * as THREE from "three";
+  import { Water } from "three/addons/objects/Water.js";
+  import { Sky } from "three/addons/objects/Sky.js";
 
   const scene = new THREE.Scene();
-
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -12,6 +13,9 @@
     1000
   );
   camera.position.z = 5;
+
+  let sun;
+  sun = new THREE.Vector3();
 
   const geometry = new THREE.BoxGeometry();
 
@@ -22,6 +26,43 @@
 
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
+
+  const water = new Water(waterGeometry, waterMaterial);
+  water.rotation.x = -Math.PI / 2;
+  scene.add(water);
+
+  const sky = new Sky();
+  sky.scale.setScalar(10000);
+  scene.add(sky);
+
+  const skyUniforms = sky.material.uniforms;
+
+  skyUniforms["turbidity"].value = 10;
+  skyUniforms["rayleigh"].value = 2;
+  skyUniforms["mieCoefficient"].value = 0.005;
+  skyUniforms["mieDirectionalG"].value = 0.8;
+
+  const updateSun = () => {
+    let renderTarget;
+    const parameters = {
+      elevation: 2,
+      azimuth: 180,
+    };
+    const pmremGenerator = new THREE.PMREMGenerator(renderer);
+    const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+    const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    sky.material.uniforms["sunPosition"].value.copy(sun);
+    water.material.uniforms["sunDirection"].value.copy(sun).normalize();
+
+    if (renderTarget !== undefined) renderTarget.dispose();
+
+    renderTarget = pmremGenerator.fromScene(sky);
+
+    scene.environment = renderTarget.texture;
+  };
 
   const directionalLight = new THREE.DirectionalLight(0x9090aa);
   directionalLight.position.set(-10, 10, -10).normalize();
@@ -53,6 +94,7 @@
       alpha: true,
     });
     resize();
+    updateSun();
     animate();
   };
 
