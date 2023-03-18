1948a449965291bd20054a15a8230f275fcdfbc9
yeonseong <yeonseong.dev@gmail.com>
2023-03-19 01:40:11 +0900

    DEBUG: add color for debug

--- src/routes/Benefits.svelte
+++ src/routes/Benefits.svelte
@@ -40,19 +40,27 @@
 	// TODO: load benefit from google sheet api
 </script>
 
-{#each benefits as benefit}
-	<div class="benefit">
-		<h1>회사명 : {benefit.company}</h1>
-		<h1>혜택 : {benefit.benefit}</h1>
-		<h1>혜택기간: {benefit.start_date} ~ {benefit.end_date}</h1>
-	</div>
-{/each}
+<div class="benefits_wrapper">
+	{#each benefits as benefit}
+		<div class="benefit">
+			<h1>회사명 : {benefit.company}</h1>
+			<h1>혜택 : {benefit.benefit}</h1>
+			<h1>혜택기간: {benefit.start_date} ~ {benefit.end_date}</h1>
+		</div>
+	{/each}
+</div>
 
 <style>
+	.benefits_wrapper {
+		overflow-y: scroll;
+		background-color: red;
+	}
 	.benefit {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
+		margin: 1rem;
+		background-color: blue;
 	}
 </style>
