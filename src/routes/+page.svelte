0a383a884dcba20d71b877535a79b857c06c5928
yeonseong <yeonseong.dev@gmail.com>
2023-03-19 01:23:24 +0900

    WIP: Benefits component
    add dummy data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
+<script>
+	import Benefits from "./Benefits.svelte";
+</script>
+
 <svelte:head>
 	<title>42 benefit</title>
 	// TODO: add meta description detail
 	<meta name="description" content="42 benefit" />
 </svelte:head>
 
-<h1>this is the world</h1>
+<Benefits />
---
+++ src/routes/Benefits.svelte
@@ -0,0 +1,58 @@
+<script>
+	let benefits = [
+		{
+			company: "42",
+			benefit: "42 benefit",
+			start_date: "2021-01-01",
+			end_date: "2021-12-31",
+		},
+		{
+			company: "43",
+			benefit: "43 benefit",
+			start_date: "2022-01-01",
+			end_date: "2023-12-31",
+		},
+		{
+			company: "42",
+			benefit: "42 benefit",
+			start_date: "2021-01-01",
+			end_date: "2021-12-31",
+		},
+		{
+			company: "42",
+			benefit: "42 benefit",
+			start_date: "2021-01-01",
+			end_date: "2021-12-31",
+		},
+		{
+			company: "42",
+			benefit: "42 benefit",
+			start_date: "2021-01-01",
+			end_date: "2021-12-31",
+		},
+		{
+			company: "42",
+			benefit: "42 benefit",
+			start_date: "2021-01-01",
+			end_date: "2021-12-31",
+		},
+	];
+	// TODO: load benefit from google sheet api
+</script>
+
+{#each benefits as benefit}
+	<div class="benefit">
+		<h1>회사명 : {benefit.company}</h1>
+		<h1>혜택 : {benefit.benefit}</h1>
+		<h1>혜택기간: {benefit.start_date} ~ {benefit.end_date}</h1>
+	</div>
+{/each}
+
+<style>
+	.benefit {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
