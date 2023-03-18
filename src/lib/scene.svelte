b73518b6cfa99f300daae6819d46311e6d90b997
yeonseong <yeonseong.dev@gmail.com>
2023-03-18 22:28:21 +0900

    FEAT: setPixelRatio()

--- src/lib/scene.svelte
+++ src/lib/scene.svelte
@@ -89,6 +89,7 @@
   };
 
   const resize = () => {
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
