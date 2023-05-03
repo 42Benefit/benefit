02db304f3bb8b3d872a88a537d1cc3a29fcd9dc4
yeonseong <yeonseong.dev@gmail.com>
2023-05-03 23:19:22 +0900

    REFACT: separate toggleButton

---
+++ src/atom/toggleButton.svelte
@@ -0,0 +1,66 @@
+<script>
+	/**
+     * @type {string}
+     */
+	 export let buttonName;
+	/**
+	 * @type {any}
+	 */
+	export let buttonFunction;
+</script>
+
+<label>
+	<span> {buttonName} </span>
+	<input type="checkbox" id="official" on:change={buttonFunction} />
+</label>
+
+<style>
+	label {
+		color: gray;
+	}
+
+	span {
+		all: unset;
+	}
+
+	label:hover {
+		cursor: pointer;
+	}
+
+	[type="checkbox"] {
+		appearance: none;
+		position: relative;
+		border: max(2px, 0.1rem) solid gray;
+		border-radius: 1.25rem;
+		width: 2.25rem;
+		height: 1.25rem;
+		top: 0.4rem;
+	}
+
+	[type="checkbox"]::before {
+		content: "";
+		position: absolute;
+		left: 0;
+		width: 1rem;
+		height: 1rem;
+		border-radius: 50%;
+		transform: scale(0.8);
+		background-color: gray;
+		transition: left 250ms linear;
+	}
+
+	[type="checkbox"]:checked::before {
+		background-color: white;
+		left: 1rem;
+	}
+
+	[type="checkbox"]:checked {
+		background-color: rgb(10, 115, 66);
+		border-color: rgb(10, 115, 66);
+	}
+
+	[type="checkbox"]:focus-visible {
+		outline-offset: max(2px, 0.1rem);
+		outline: max(2px, 0.1rem) solid rgb(10, 115, 66);
+	}
+</style>
--- src/component/Benefit/Benefits.svelte
+++ src/component/Benefit/Benefits.svelte
@@ -1,10 +1,10 @@
 <script>
 	// @ts-nocheck
-
 	import benefits from "../../data/data.json";
 	import studentBenefits from "../../data/student.json";
 	import Benefit from "./Benefit.svelte";
 	import NoBenefit from "./NoBenefit.svelte";
+	import ToggleButton from "../../atom/toggleButton.svelte";
 
 	//TODO: Refactor this code, separate into modules
 
@@ -175,14 +175,10 @@
 				on:click={changeCategory}>기타</button
 			>
 		</div>
-		<label>
-			<span> official </span>
-			<input
-				type="checkbox"
-				id="official"
-				on:change={toggleOfficialBenefits}
-			/>
-		</label>
+		<ToggleButton
+			buttonName="official"
+			buttonFunction={toggleOfficialBenefits}
+		/>
 	</header>
 	<div class="benefits-list">
 		{#each showBenefits as benefit}
@@ -228,55 +224,6 @@
 		all: unset;
 		margin: 0.42rem;
 		opacity: 0.42;
-	}
-
-	.category label {
-		color: gray;
-	}
-
-	.category span {
-		all: unset;
-	}
-
-	.category label:hover {
-		cursor: pointer;
-	}
-
-	[type="checkbox"] {
-		appearance: none;
-		position: relative;
-		border: max(2px, 0.1rem) solid gray;
-		border-radius: 1.25rem;
-		width: 2.25rem;
-		height: 1.25rem;
-		top: 0.4rem;
-	}
-
-	[type="checkbox"]::before {
-		content: "";
-		position: absolute;
-		left: 0;
-		width: 1rem;
-		height: 1rem;
-		border-radius: 50%;
-		transform: scale(0.8);
-		background-color: gray;
-		transition: left 250ms linear;
-	}
-
-	[type="checkbox"]:checked::before {
-		background-color: white;
-		left: 1rem;
-	}
-
-	[type="checkbox"]:checked {
-		background-color: rgb(10, 115, 66);
-		border-color: rgb(10, 115, 66);
-	}
-
-	[type="checkbox"]:focus-visible {
-		outline-offset: max(2px, 0.1rem);
-		outline: max(2px, 0.1rem) solid rgb(10, 115, 66);
 	}
 
 	:global(.show) {
