f3942dc08a2801efd58c707c419539c627be2d0c
yeonseong <yeonseong.dev@gmail.com>
2023-05-03 23:41:39 +0900

    FEAT: add delay typing `...` in NoBenefit

--- src/component/Benefit/NoBenefit.svelte
+++ src/component/Benefit/NoBenefit.svelte
@@ -10,20 +10,20 @@
 			waitUntilVisible: true,
 			startDelay: 1000,
 			cursor: false,
-		}).go();
+		}).go().pause(1000).type(" .").pause(500).type(".").pause(500).type(".");
 
 		// @ts-ignore
 		new TypeIt(".no-benefit #second", {
 			speed: 42,
 			waitUntilVisible: true,
-			startDelay: 2000,
+			startDelay: 5000,
 			cursor: false,
-		}).go();
+		}).go().pause(420).type("!");
 	});
 </script>
 
 <section class="no-benefit">
-	<i id="first">여행은 언제나 고독한 법이죠...</i>
+	<i id="first">여행은 언제나 고독한 법이죠</i>
 	<i id="second">베네핏 없이도 항해를 이어나갈 당신을 응원합니다.</i>
 </section>
 
