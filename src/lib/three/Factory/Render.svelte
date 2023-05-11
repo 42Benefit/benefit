9b10c39130c70f7d300a6e340f95b003cf5e78f6
yeonseong <yeonseong.dev@gmail.com>
2023-05-12 00:10:26 +0900

    REFACT: separate RendererFactory

---
+++ src/lib/three/Factory/Render.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+  // @ts-nocheck
+  import * as THREE from "three";
+
+  export const RendererFactory = () => {
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true,
+    });
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+
+    return renderer;
+  };
+</script>
